e83d9bdd32fbc7b75bd2a340b4fa3c8dd30b8211
Nicolas-Boltz <boltz.nicolas@gmx.de>
2021-05-19 17:32:20 +0200

    Reverting changes to already existing OLD test files

--- de.sebinside.dcp.dsl.tests/resultmapping/accesscontrol-palladio/constraint.pl
+++ de.sebinside.dcp.dsl.tests/resultmapping/accesscontrol-palladio/constraint.pl
@@ -11,22 +11,3 @@ constraint_AuthorizedAccess_PreCallState(QueryType, OP, S, ST, VarSet_authRoles,
 	findall(IteratorTemplate, operationProperty(OP, 'EnumCharacteristicType Roles (_vP5JoFqnEeiY18w7ObeSrg)', IteratorTemplate), VarSet_accessRoles),
 	( intersection(VarSet_authRoles, VarSet_accessRoles, Temp_0),
 	\+ length(Temp_0, 0) ).
-	
-	
-	
-	
-	Optional[constraint_AuthorizedAccess(ConstraintName, QueryType, OP, S, ST, VarSet_accessRoles, VarSet_authRoles) :-
-	ConstraintName = 'AuthorizedAccess',
-	constraint_AuthorizedAccess_PreCallState(QueryType, OP, S, ST, VarSet_accessRoles, VarSet_authRoles).
-constraint_AuthorizedAccess_PreCallState(QueryType, OP, S, ST, VarSet_accessRoles, VarSet_authRoles) :-
-	QueryType = 'PreCallState',
-	S = [OP | _],
-	stackValid(S),
-	operationState(OP, ST),
-	findall(IteratorTemplate, preCallState(S, OP, ST, 'Optional[AccessPermissions (_k9jB49vTEeqNdo_V4bA-xw)]', IteratorTemplate), VarSet_authRoles),
-	findall(IteratorTemplate, operationProperty(OP, 'Optional[Roles (_JvuuQ9vqEeqNdo_V4bA-xw)]', IteratorTemplate), VarSet_accessRoles),
-	(
-		intersection(VarSet_authRoles, VarSet_accessRoles, Temp_0),
-		\+ length(Temp_0, 0)
-	).
-]
--- de.sebinside.dcp.dsl.tests/resultmapping/shop-class/constraint.pl
+++ de.sebinside.dcp.dsl.tests/resultmapping/shop-class/constraint.pl
@@ -2,15 +2,12 @@ characteristicsClass_isNotSafe_location_0_NEG('EU').
 characteristicsClass_isNotSafe(ClassVar_isNotSafe_location) :-
 	valueSetMember('Locations', ClassVar_isNotSafe_location),
 	\+ characteristicsClass_isNotSafe_location_0_NEG(ClassVar_isNotSafe_location).
-	
 constraint_NoType0Flow(ConstraintName, QueryType, OP, S, ST, ClassVar_isNotSafe_location, P) :-
 	ConstraintName = 'NoType0Flow',
 	( constraint_NoType0Flow_PreCallState(QueryType, OP, S, ST, ClassVar_isNotSafe_location);
 	constraint_NoType0Flow_PostCallState(QueryType, OP, S, ST, ClassVar_isNotSafe_location);
 	constraint_NoType0Flow_CallArgument(QueryType, OP, S, P, ClassVar_isNotSafe_location);
 	constraint_NoType0Flow_ReturnValue(QueryType, OP, S, P, ClassVar_isNotSafe_location) ).
-	
-	
 constraint_NoType0Flow_PreCallState(QueryType, OP, S, ST, ClassVar_isNotSafe_location) :-
 	QueryType = 'PreCallState',
 	S = [OP | _],
@@ -19,7 +16,6 @@ constraint_NoType0Flow_PreCallState(QueryType, OP, S, ST, ClassVar_isNotSafe_loc
 	preCallState(S, OP, ST, 'level', 'Type-0'),
 	operationProperty(OP, 'location', ClassVar_isNotSafe_location),
 	characteristicsClass_isNotSafe(ClassVar_isNotSafe_location).
-	
 constraint_NoType0Flow_PostCallState(QueryType, OP, S, ST, ClassVar_isNotSafe_location) :-
 	QueryType = 'PostCallState',
 	S = [OP | _],
@@ -28,7 +24,6 @@ constraint_NoType0Flow_PostCallState(QueryType, OP, S, ST, ClassVar_isNotSafe_lo
 	postCallState(S, OP, ST, 'level', 'Type-0'),
 	operationProperty(OP, 'location', ClassVar_isNotSafe_location),
 	characteristicsClass_isNotSafe(ClassVar_isNotSafe_location).
-	
 constraint_NoType0Flow_CallArgument(QueryType, OP, S, P, ClassVar_isNotSafe_location) :-
 	QueryType = 'CallArgument',
 	S = [OP | _],
@@ -37,7 +32,6 @@ constraint_NoType0Flow_CallArgument(QueryType, OP, S, P, ClassVar_isNotSafe_loca
 	callArgument(S, P, 'level', 'Type-0'),
 	operationProperty(OP, 'location', ClassVar_isNotSafe_location),
 	characteristicsClass_isNotSafe(ClassVar_isNotSafe_location).
-
 constraint_NoType0Flow_ReturnValue(QueryType, OP, S, P, ClassVar_isNotSafe_location) :-
 	QueryType = 'ReturnValue',
 	S = [OP | _],

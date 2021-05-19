396af1b5dec2121cf0e218d6b3cbc009097fbc6b
Nicolas-Boltz <boltz.nicolas@gmx.de>
2021-05-19 17:32:20 +0200

    Reverting changes to already existing OLD test files

--- de.sebinside.dcp.dsl.tests/resultmapping/accesscontrol-simple/constraint.pl
+++ de.sebinside.dcp.dsl.tests/resultmapping/accesscontrol-simple/constraint.pl
@@ -22,12 +22,3 @@ constraint_AuthorizedAccess_ReturnValue(QueryType, OP, S, P, VarSet_authRoles, V
 	findall(IteratorTemplate, operationProperty(OP, 'accessRoles', IteratorTemplate), VarSet_accessRoles),
 	( intersection(VarSet_authRoles, VarSet_accessRoles, Temp_0),
 	\+ length(Temp_0, 0) ).
-	
-	
-constraint_AuthorizedAccess_Input(QueryType, N, PIN, S, VarSet_authRoles, VarSet_accessRoles) :-
-	QueryType = 'Input',
-	inputPin(N, PIN), 
-	setof(R, nodeCharacteristic(N, 'Roles (_JvuuQ9vqEeqNdo_V4bA-xw)', R), VarSet_authRoles),
-	setof_characteristics(N, PIN, 'AccessPermissions (_k9jB49vTEeqNdo_V4bA-xw)', VarSet_accessRoles, S),
-	intersection(VarSet_authRoles, VarSet_accessRoles, []).
-	flowTree(N, PIN, S).
